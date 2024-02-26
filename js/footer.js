6cad171d7d97870fc584a84ec5663f1cc32edd5c
HalynaMal <dguoit@ukr.net>
2024-02-26 23:04:00 +0200

    add footer

---
+++ js/footer.js
@@ -0,0 +1,61 @@
+const STORAGE_KEY = "feedback-form-footer";
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", () => {
+    const userEmail = form.elements.email.value;
+    
+    const data = {
+    email: userEmail,   
+    };
+
+  saveToLS(STORAGE_KEY, data);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // валідація поля email
+  const userEmail = form.elements.email.value;
+  
+  if (userEmail.trim() === '') {
+      // alert('Будь ласка, введіть email.');
+      Swal.fire({
+        title: "Будь ласка, введіть Ваш email",
+        // text: "Це може бути кращим варіантом для кастомізації і стилізації.",
+        icon: "warning", // Може бути 'success', 'error', 'warning', 'info', тощо
+        confirmButtonText: "ОК",
+      });
+    return;
+  }
+// ====================
+  const data = loadFromLS(STORAGE_KEY) || {};
+  console.log(data);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
+
+function loadFromLS(key = "empty") {
+  const data = localStorage.getItem(key);
+
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch {
+    return data;
+  }
+}
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function restoreData() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+
+  form.elements.email.value = data.email || "";
+}
+
+restoreData();
